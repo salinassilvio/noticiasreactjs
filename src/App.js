5f5312ea5d4ac43c6c9b6abfbf9197167a6e1a9a
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2021-05-26 23:07:52 -0600

    Formulario y creacion de modulos css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
+
 function App() {
   return (
     <Fragment>
       <Header titulo='Buscador de Noticias'></Header>
       <div className="container white">
-        
+        <Formulario></Formulario>
       </div>
     </Fragment>
   );
