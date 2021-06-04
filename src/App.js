9b6d0a3724410e34489af3c1ffc6b86e3031a818
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2021-06-03 23:03:34 -0600

    se agrega useEffect para obtener las noticias por categoria

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState,useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  //definir la categoria y noticias
+  const [categoria,guardarCategoria] = useState('');
+
+  //Para cuando detecte un cambio
+  useEffect(()=>{
+    const consultarAPI = async () =>{
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&
+      apiKey=1c59f04d40114c2ba637d389d4452b6f`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      console.log(noticias);
+    }
+    consultarAPI();
+  },[categoria]);
+
   return (
     <Fragment>
-      <Header titulo='Buscador de Noticias'></Header>
+      <Header 
+        titulo='Buscador de Noticias'></Header>
       <div className="container white">
-        <Formulario></Formulario>
+        <Formulario
+          guardarCategoria={guardarCategoria}
+        ></Formulario>
       </div>
     </Fragment>
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,15 +2,35 @@ import React from 'react';
 import styles from './Formulario.module.css';
 import useSelect  from "../hooks/useSelect";
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
+
+    //opciones
+    const Opciones = [
+        {value:'general', label:'General'},
+        {value:'business', label:'Negocios'},
+        {value:'entertaiment', label:'Entretenimiento'},
+        {value:'health', label:'Salud'},
+        {value:'science', label:'Ciencia'},
+        {value:'sports', label:'Deporte'},
+        {value:'technology', label:'Tecnologia'},
+    ]
 
     //Utilizar custom hook
-    const [categoria, SelectNoticias] = useSelect();
+    const [categoria, SelectNoticias] = useSelect('general',Opciones);
+
+    //Submit al form, pasar categoria a app.js
+    const buscarNoticias = e =>{
+        e.preventDefault();
+
+        guardarCategoria(categoria);
+    }
 
     return ( 
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
-                <form>
+                <form
+                    onSubmit={buscarNoticias}
+                >
                     <h2 className={styles.heading}>Encuentra Noticias por Categoria</h2>
                    
                    <SelectNoticias></SelectNoticias>
